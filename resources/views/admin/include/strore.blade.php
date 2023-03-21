424c03afe973943947aeae2cbd0e1a6d50b5cca6
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2023-03-21 23:19:21 +0600

    updated

--- resources/views/admin/include/strore.blade.php
+++
@@ -1,7 +0,0 @@
-<a href="#" onclick="event.preventDefault(); document.getElementById('delete').submit()">
-    <form action="{{ route('delete') }}" method="post" id="delete">
-        @csrf
-        <input type="hidden" value="{{ $product->id }}" name="product_id">
-        <input type="submit" value="Delete" class="btn btn-danger">
-    </form>
-</a>
--- storage/framework/views/f8accf07a02d347febdba8a2130b4afcf61e8841.php
+++
@@ -1,6 +0,0 @@
-<div class="hidden sm:block">
-    <div class="py-8">
-        <div class="border-t border-gray-200"></div>
-    </div>
-</div>
-<?php /**PATH D:\SWE Training\xampp-BITM\htdocs\eBookShare\vendor\laravel\jetstream\src/../resources/views/components/section-border.blade.php ENDPATH**/ ?>
